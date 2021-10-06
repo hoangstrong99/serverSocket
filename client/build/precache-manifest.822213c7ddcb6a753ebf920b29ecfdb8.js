7f8e4f48c3ee41980be2f737b1246a4bd02996b7
hoangstrong99 <hoangducmanh99@gmail.com>
2021-10-06 14:44:15 +0700

    add client

---
+++ client/build/precache-manifest.822213c7ddcb6a753ebf920b29ecfdb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a4204393c12750abc35fd84ffc6c95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c13881387e45faa074f9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3edf9b815c2cced8b782",
+    "url": "/static/js/2.144eac2f.chunk.js"
+  },
+  {
+    "revision": "a94751185011317bdf3fabb99649b8a5",
+    "url": "/static/js/2.144eac2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13881387e45faa074f9",
+    "url": "/static/js/main.4aa5e446.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
